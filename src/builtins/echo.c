26176787056f8c0a1bf5e87e2234c81b5425a4a7
Leonid Poroshin <lporoshi@student.42heilbronn.de>
2023-12-10 16:33:20 +0100

    free memory before every return

--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -6,7 +6,7 @@
 /*   By: lporoshi <lporoshi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/09 17:32:34 by lporoshi          #+#    #+#             */
-/*   Updated: 2023/12/10 16:22:21 by lporoshi         ###   ########.fr       */
+/*   Updated: 2023/12/10 16:32:56 by lporoshi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,10 +22,18 @@ int	echo(char **argv, bool no_newline_flag)
 	if (output_string == NULL)
 		return (FT_ERROR);
 	if (ft_putstr_fd(output_string, STDOUT_FILENO) == FT_ERROR)
+	{
+		free(output_string);
 		return (FT_ERROR);
+	}
 	if (no_newline_flag == false)
+	{
 		if (ft_putchar_fd('\n', STDOUT_FILENO) == FT_ERROR)
+		{
+			free(output_string);
 			return (FT_ERROR);
+		}
+	}
 	free(output_string);
 	return (FT_SUCCESS);
 }
