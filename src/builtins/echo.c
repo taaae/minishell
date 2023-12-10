52df0ddea2014f6f922c17dc6a52cb9cbc33a221
Leonid Poroshin <lporoshi@student.42heilbronn.de>
2023-12-10 13:46:06 +0100

    snapshot

--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -6,12 +6,15 @@
 /*   By: lporoshi <lporoshi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/09 17:32:34 by lporoshi          #+#    #+#             */
-/*   Updated: 2023/12/09 18:04:53 by lporoshi         ###   ########.fr       */
+/*   Updated: 2023/12/10 13:19:28 by lporoshi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 #include "../../include/builtins.h"
+
+//Echo will behave like a program w/ main() function probably
+//So it'll have argc, argv as args (?)
 
 // static size_t	get_str_arr_size(char **lines)
 // {
