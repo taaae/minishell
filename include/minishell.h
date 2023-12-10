8146c6c7666bca77196341aaae6386028c61abcd
Leonid Poroshin <lporoshi@student.42heilbronn.de>
2023-12-10 15:16:15 +0100

    remove t_bool from minishell.h

--- include/minishell.h
+++ include/minishell.h
@@ -6,16 +6,11 @@
 /*   By: lporoshi <lporoshi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/09 17:39:32 by lporoshi          #+#    #+#             */
-/*   Updated: 2023/12/09 17:54:03 by lporoshi         ###   ########.fr       */
+/*   Updated: 2023/12/10 15:07:07 by lporoshi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
-
-typedef enum e_bool{
-	false,
-	true
-}	t_bool;
 
 #endif
