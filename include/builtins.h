085243e650804cda7eea76becd2282835580c153
Leonid Poroshin <lporoshi@student.42heilbronn.de>
2023-12-12 18:59:34 +0100

    make reader read until a valid line is parsed, give an error for unclosed brackets

--- include/builtins.h
+++ include/builtins.h
@@ -6,12 +6,14 @@
 /*   By: lporoshi <lporoshi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/09 17:31:39 by lporoshi          #+#    #+#             */
-/*   Updated: 2023/12/10 19:44:31 by lporoshi         ###   ########.fr       */
+/*   Updated: 2023/12/12 18:46:28 by lporoshi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BUILTINS_H
 # define BUILTINS_H
+
+# include <stdbool.h>
 
 int	builtin_echo(char **argv, bool no_newline_flag);
 int	builtin_pwd(char **argv);
--- src/reader/reader.c
+++ src/reader/reader.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   reader.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: trusanov <trusanov@student.42.fr>          +#+  +:+       +#+        */
+/*   By: lporoshi <lporoshi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/09 15:15:07 by lporoshi          #+#    #+#             */
-/*   Updated: 2023/12/10 14:03:00 by trusanov         ###   ########.fr       */
+/*   Updated: 2023/12/12 18:58:35 by lporoshi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 #include <stdlib.h>
 #include <readline/readline.h>
 #include <readline/history.h>
+#include <stdbool.h>
+#include "libft.h"
 #include "reader.h"
 #include "config.h"
 
@@ -27,13 +29,51 @@ void	close_reader(void)
 	rl_clear_history();
 }
 
+static bool	unclosed_brackets(char *line)
+{
+	bool	unclosed_quote;
+	bool	unclosed_dquote;
+	int		unclosed_parenth;
+
+	unclosed_quote = false;
+	unclosed_dquote = false;
+	unclosed_parenth = 0;
+	while (*line)
+	{
+		if (*line == '\'')
+			unclosed_quote = !unclosed_quote;
+		else if (*line == '"')
+			unclosed_dquote = !unclosed_dquote;
+		else if (*line == '(')
+			unclosed_parenth += 1;
+		else if (*line == ')')
+			unclosed_parenth -= 1;
+	}
+	return (unclosed_quote == true || unclosed_dquote == true \
+	|| unclosed_parenth != 0);
+}
+
 char	*get_line(void)
 {
 	char			*line;
 
-	line = readline(SHELL_PROMPT);
-	if (line == NULL || line[0] == '\0')
-		return (NULL);
-	add_history(line);
-	return (line);
+	while (1)
+	{
+		line = readline(SHELL_PROMPT);
+		if (line == NULL)
+			return (NULL);
+		if (line[0] == '\0')
+		{
+			free(line);
+			continue ;
+		}
+		if (unclosed_brackets(line) == true)
+		{
+			free(line);
+			ft_putstr_fd("Unclosed quotes or brackets!\n", STDERR_FILENO);
+			continue ;
+		}
+		add_history(line);
+		return (line);
+	}
 }
