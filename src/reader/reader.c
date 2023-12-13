2988bb8aa7be2dbad6b30906585ec56456a9c1c0
Leonid Poroshin <lporoshi@student.42heilbronn.de>
2023-12-13 12:46:06 +0100

    finish the reader

--- src/reader/reader.c
+++ src/reader/reader.c
@@ -6,7 +6,7 @@
 /*   By: lporoshi <lporoshi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/09 15:15:07 by lporoshi          #+#    #+#             */
-/*   Updated: 2023/12/12 18:58:35 by lporoshi         ###   ########.fr       */
+/*   Updated: 2023/12/13 12:44:59 by lporoshi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,30 +29,6 @@ void	close_reader(void)
 	rl_clear_history();
 }
 
-static bool	unclosed_brackets(char *line)
-{
-	bool	unclosed_quote;
-	bool	unclosed_dquote;
-	int		unclosed_parenth;
-
-	unclosed_quote = false;
-	unclosed_dquote = false;
-	unclosed_parenth = 0;
-	while (*line)
-	{
-		if (*line == '\'')
-			unclosed_quote = !unclosed_quote;
-		else if (*line == '"')
-			unclosed_dquote = !unclosed_dquote;
-		else if (*line == '(')
-			unclosed_parenth += 1;
-		else if (*line == ')')
-			unclosed_parenth -= 1;
-	}
-	return (unclosed_quote == true || unclosed_dquote == true \
-	|| unclosed_parenth != 0);
-}
-
 char	*get_line(void)
 {
 	char			*line;
@@ -65,12 +41,6 @@ char	*get_line(void)
 		if (line[0] == '\0')
 		{
 			free(line);
-			continue ;
-		}
-		if (unclosed_brackets(line) == true)
-		{
-			free(line);
-			ft_putstr_fd("Unclosed quotes or brackets!\n", STDERR_FILENO);
 			continue ;
 		}
 		add_history(line);
