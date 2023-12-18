68e18271e0d5638b992e551fca98fd92781ade94
timkingteam <timkingteam@gmail.com>
2023-12-18 18:45:13 +0100

    migrated environment tests

---
+++ test/environment_test.c
@@ -0,0 +1,110 @@
+#include "unity.h"
+#include "libft.h"
+#include "environment.h"
+#include "builtins.h"
+
+void setUp(void) {
+}
+
+void tearDown(void) {
+}
+
+void test_environment_core(void) {
+  char *to_free;
+  char *envp[] = {"PATH=/usr/local/bin:/usr/bin", "COLORTERM=truecolor", NULL};
+  ft_initenv(envp);
+  TEST_ASSERT_EQUAL_STRING("/usr/local/bin:/usr/bin", to_free = ft_getenv("PATH"));
+  free(to_free);
+  TEST_ASSERT_EQUAL_STRING("truecolor", to_free = ft_getenv("COLORTERM"));
+  free(to_free);
+  TEST_ASSERT_EQUAL(NULL, ft_getenv("missing"));
+  ft_setenv("newvar", "newval");
+  TEST_ASSERT_EQUAL_STRING("newval", to_free = ft_getenv("newvar"));
+  free(to_free);
+  ft_unsetenv("COLORTERM");
+  TEST_ASSERT_EQUAL(NULL, ft_getenv("COLORTERM"));
+  TEST_ASSERT_EQUAL(-1, ft_setenv("a=b", "c"));
+  TEST_ASSERT_EQUAL(NULL, ft_getenv("a=b"));
+  char **environ = get_environ();
+  int i = 0;
+  for (; environ[i] != NULL; i++) {}
+  TEST_ASSERT_EQUAL(2, i);
+  TEST_ASSERT_EQUAL(0, ft_unsetenv("missign"));
+  TEST_ASSERT_EQUAL(-1, ft_unsetenv("inva=lid"));
+  TEST_ASSERT_EQUAL(-1, ft_unsetenv(""));
+  ft_unsetenv("PATH");
+  ft_unsetenv("newvar");
+  ft_setenv("abc", "val=with=equal");
+  TEST_ASSERT_EQUAL_STRING("val=with=equal", to_free = ft_getenv("abc"));
+  free(to_free);
+  ft_unsetenv("abc");
+  TEST_ASSERT_EQUAL(get_environ()[0], NULL);
+  free(get_environ());
+}
+
+void test_builtin_env(void) {
+  char *envp[] = {"PATH=/usr/local/bin:/usr/bin", "COLORTERM=truecolor", NULL};
+  ft_initenv(envp);
+  char *empty_argv[] = {NULL};
+  ft_printf("\nbuiltin_env test:\n");
+  builtin_env(empty_argv);
+  empty_argv[0] = "abc";
+  builtin_env(empty_argv);
+  empty_argv[0] = NULL;
+  ft_unsetenv("PATH");
+  ft_unsetenv("COLORTERM");
+  builtin_env(empty_argv);
+  free(get_environ());
+}
+
+void test_builtin_unset(void) {
+  char *envp[] = {"PATH=/usr/local/bin:/usr/bin", "COLORTERM=truecolor", "USER=root", NULL};
+  ft_initenv(envp);
+  char *empty_argv[] = {NULL};
+  ft_printf("\n\n\nbuiltin_unset test:\n");
+  char *unset_argv[] = {"nonexisting", "er=ror", NULL};
+  TEST_ASSERT_EQUAL(1, builtin_unset(unset_argv));
+  builtin_env(empty_argv);
+  unset_argv[0] = "PATH";
+  unset_argv[1] = "USER";
+  ft_printf("\n");
+  TEST_ASSERT_EQUAL(0, builtin_unset(unset_argv));
+  builtin_env(empty_argv);
+  unset_argv[0] = "";
+  TEST_ASSERT_EQUAL(1, builtin_unset(unset_argv));
+  unset_argv[0] = "COLORTERM";
+  builtin_unset(unset_argv);
+  free(get_environ());
+}
+
+void test_builtin_export(void) {
+  char *envp[] = {"PATH=/usr/local/bin:/usr/bin", "COLORTERM=truecolor", "USER=root", NULL};
+  ft_initenv(envp);
+  char *empty_argv[] = {NULL};
+  ft_printf("\n\n\nbuiltin_export test:\n");
+  char *argv[] = {"aaa=bbb", "ccc", NULL};
+  TEST_ASSERT_EQUAL(0, builtin_export(argv));
+  builtin_env(empty_argv);
+  argv[0] = "PATH";
+  argv[1] = "COLORTERM";
+  builtin_unset(argv);
+  ft_printf("\n");
+  builtin_env(empty_argv);
+  argv[0] = "USER=grot";
+  argv[1] = "";
+  TEST_ASSERT_EQUAL(1, builtin_export(argv));
+  ft_printf("\n");
+  builtin_env(empty_argv);
+}
+
+void test_env_error_messages(void) {
+  ft_printf("\n\ntest env error messages:\n");
+  ft_printf("for unset:\n");
+  char *argv[] = {" ", "", "a=b", "=2", "=", " a", "2a", "_", "?^@", NULL};
+  builtin_unset(argv);
+  ft_printf("\nfor export:\n");
+  char *argv2[] = {" ", "", "2a=b", " a=b", " b", "_a=b=c", " _a", "?^", NULL};
+  builtin_export(argv2);
+  char *empty_argv[] = {NULL};
+  builtin_env(empty_argv);
+}
