b134d66dfebdeb919206ef048bb4989c8a4bcbd0
Leonid Poroshin <lporoshi@student.42heilbronn.de>
2023-12-18 11:32:53 +0100

    need to change branch

--- test/sig_test.c
+++ test/sig_test.c
@@ -1,34 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <signal.h>
+#include <unistd.h>
 #include <readline/readline.h>
 #include <readline/history.h>
+#include <sys/ioctl.h>
+#include "../libft/libft.h"
 
-void sigintHandler(int sig) {
-    // Clear the current line
-    rl_redisplay();
-    printf("\n");
-    // Display a new prompt
-    rl_on_new_line();
-    rl_replace_line("", 0);
-    rl_redisplay();
-}
 
-int	init_signals(void)
-{
-    signal(SIGINT, sigintHandler);
-	signal(SIGQUIT, SIG_IGN);
-}
 
 // Function to handle Ctrl+C signal (SIGINT)
 void sigintHandler(int sig) {
     // Clear the current line
-    rl_redisplay();
-    printf("\n");
-    // Display a new prompt
-    rl_on_new_line();
-    rl_replace_line("", 0);
-    rl_redisplay();
+    // rl_redisplay();
+    // printf("\n");
+    // // Display a new prompt
+    // rl_on_new_line();
+    // rl_replace_line("", 0);
+    // rl_redisplay();
+	if (sig == SIGINT)
+	{
+		rl_redisplay();
+		write(1, "\n", 1);
+		rl_on_new_line();
+		rl_replace_line("", 0);
+		rl_redisplay();
+	}
+	return ;
 }
 
 int main() {
@@ -36,6 +34,8 @@ int main() {
     signal(SIGINT, sigintHandler);
 	signal(SIGQUIT, SIG_IGN);
 
+
+    rl_catch_signals = 0;
     // Main loop for reading input lines
     while (1) {
         // Use the GNU Readline library to get a line of input
