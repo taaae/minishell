444f929e7ab1a11c3f9c45bf7674d2052608ced2
Leonid Poroshin <lporoshi@student.42heilbronn.de>
2024-01-22 11:48:03 +0100

    rework echo: exit codes like a prog, perror msgs

--- src/builtins/builtin_echo.c
+++ src/builtins/builtin_echo.c
@@ -6,7 +6,7 @@
 /*   By: lporoshi <lporoshi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/09 17:32:34 by lporoshi          #+#    #+#             */
-/*   Updated: 2023/12/19 15:53:10 by lporoshi         ###   ########.fr       */
+/*   Updated: 2024/01/22 11:46:06 by lporoshi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,22 +17,24 @@
 static int	echo_printer(char *s, bool no_nl)
 {
 	if (s == NULL)
-		return (FT_ERROR);
-	if (ft_putstr_fd(s, STDOUT_FILENO) == FT_ERROR)
+		return (EXIT_FAILURE);
+	if (ft_putstr_fd(s, STDOUT_FILENO) != ft_strlen(s))
 	{
+		perror("ft_printf error");
 		free(s);
-		return (FT_ERROR);
+		return (EXIT_FAILURE);
 	}
 	if (no_nl == false)
 	{
-		if (ft_putchar_fd('\n', STDOUT_FILENO) == FT_ERROR)
+		if (ft_putchar_fd('\n', STDOUT_FILENO) != 1)
 		{
+			perror("ft_printf error");
 			free(s);
-			return (FT_ERROR);
+			return (EXIT_FAILURE);
 		}
 	}
 	free(s);
-	return (FT_SUCCESS);
+	return (EXIT_SUCCESS);
 }
 
 int	builtin_echo(int argc, char **argv)
@@ -40,9 +42,8 @@ int	builtin_echo(int argc, char **argv)
 	char	*output_string;
 	bool	no_newline;
 
-	(void)argc;
-	if (argv == NULL || argv[0] == NULL)
-		return (FT_SUCCESS);
+	if (argc < 1 || argv == NULL || argv[0] == NULL)
+		return (EXIT_FAILURE);
 	no_newline = false;
 	if (argv[1] != NULL && !ft_strcmp(argv[1], "-n"))
 	{
