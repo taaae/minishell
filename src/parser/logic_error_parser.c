fa787d5c23a3ec32cec66036afedb069f3e1096e
taaae <timkingteam@gmail.com>
2024-01-23 19:33:29 +0100

    error parser (seems to work)

--- src/parser/logic_error_parser.c
+++ src/parser/logic_error_parser.c
@@ -1,36 +1,39 @@
 #include <stddef.h>
+#include "libft.h"
 #include "parser.h"
 #include "logic_tokenizer.h"
 
 const char    *token_error(const t_logic_token *token)
 {
     if (token == NULL)
-        return "minishell: unexptected empty token";
+        return ft_strdup("minishell: unexptected empty token");
     if (token->type == OPEN_PAR)
-        return "minishell: syntax error near unexpected token `('";
+        return ft_strdup("minishell: syntax error near unexpected token `('");
     if (token->type == CLOS_PAR)
-        return "minishell: syntax error near unexpected token `)'";
+        return ft_strdup("minishell: syntax error near unexpected token `)'");
     if (token->type == OR_SYM)
-        return "minishell: syntax error near unexpected token `||'";
+        return ft_strdup("minishell: syntax error near unexpected token `||'");
     if (token->type == AND_SYM)
-        return "minishell: syntax error near unexpected token `&&'";
+        return ft_strdup("minishell: syntax error near unexpected token `&&'");
     if (token->type == SCRIPT_STR)
-        return "minishell: syntax error near unexpected token script_str";
-    return "minishell: unexptected token type";
+        return ft_strjoin("minishell: syntax error near unexpected token: ", token->strrepr);
+    return ft_strdup("minishell: unexptected token type");
 }
 
 const char    *parse_check_expr(t_logic_token ***token);
 const char    *parse_check_term(t_logic_token ***token);
 
-// Expr -> Term{('&&', '||')Term}
+// Expr -> Term{('&&', '||')Expr}
 // Term -> '(' Expr ')' | script_str
 // Pass first element of NULL terminated token array from logic_split func
-// Returns NULL on success or stack-allocated string with error description if fail
+// Returns NULL on success or dynamically allocated string with error description if fail
 #include <stdio.h>
 const char    *parse_check(t_logic_token **token)
 {
     const char    *res;
 
+    if (token == NULL)
+        return ft_strdup("minishell: unclosed quote");
     res = parse_check_expr(&token);
     if (res != NULL)
         return (res);
@@ -51,9 +54,9 @@ const char    *parse_check_expr(t_logic_token ***token)
     if ((**token)->type == AND_SYM || (**token)->type == OR_SYM)
     {
         (*token)++;
-        return (parse_check_term(token));
+        return (parse_check_expr(token));
     }
-    return (token_error(**token));
+    return (NULL);
 }
 
 const char    *parse_check_term(t_logic_token ***token)
@@ -64,6 +67,7 @@ const char    *parse_check_term(t_logic_token ***token)
         return (token_error(NULL));
     if ((**token)->type == SCRIPT_STR)
     {
+        // here should be parse_check_pipeline
         (*token)++;
         return (NULL);
     }
--- test/logic_error_parser_test.c
+++ test/logic_error_parser_test.c
@@ -7,12 +7,58 @@
 int main()
 {
     char *s[] = {
+            "echo abc",
+            "echo",
             "echo abc && )",
+            "&&",
+            "echo abc &&",
+            "||",
+            "()",
+            "(echo abc) | echo",
+            "",
+            "echo aaa && ()",
+            "((",
+            "echo a && echo b && echo c",
+            "(echo a && echo b)",
+            "''",
+            "'",
+            "(echo hello",
+            "echo world))",
+            "&& echo hello",
+            "echo world ||",
+            "echo a && || echo b",
+            "echo a || && echo b",
+            "echo hello && ||",
+            "&& && &&",
+            "&& echo hello &&",
+            "echo (())",
+            "echo hello && || && echo world",
+            "echo \"Hello",
+            "echo 'World",
+            "echo hello >",
+            "< echo world",
+            "echo hello echo world",
+            "(echo hello && (echo world || echo!))",
+            "(echo inner) && echo outer",
+            "echo this && echo that || echo those && echo these",
+            "echo 'echo nested'",
+            "((echo a && (echo b || (echo c))) && (((echo d))))",
+            "((((echo a && echo b)) || ((echo c || echo d))))",
+            "echo a && (echo b || (echo c && (echo d))) || (echo e)",
+            "((echo a || (echo b)) && echo c) || (((echo d && echo e)))",
+            "() && echo a || (()) && echo b",
+            "(((((echo a)))) && echo b) || echo c",
+            "((||) && echo a) || ((echo b &&))",
+            "((echo a || (echo b && echo c)) && ((echo d || echo e)))",
+            "(echo a || (echo b && echo c)) && ((echo d || echo e)))",
+            "((echo a || (echo b && echo c) && ((echo d || echo e)))",
+            "((echo a || (echo b && echo c)) && ((echo d || echo e))",
+            "((((echo a && echo b)) || ((echo c || echo d))",
+            "((((echo a && echo b)) || ((echo c || echo d)))",
+            "echo a && echo b)) || ((echo c || echo d",
             NULL,
     };
-    t_logic_token **a = logic_split("echo abc && )");
-    printf("%p\n", a);
     for (int i = 0; s[i] != NULL; i++) {
-        printf("input: %s ; result: %s\n", s[i], parse_check(logic_split(s[i])));
+        printf("input: %s\n result: %s\n\n", s[i], parse_check(logic_split(s[i])));
     }
 }
