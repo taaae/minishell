4e603d558778c23783c03c81bb8475fa2ccfcf75
taaae <timkingteam@gmail.com>
2024-01-29 16:49:23 +0100

    pipeline tokenizer

--- include/parser.h
+++ include/parser.h
@@ -8,4 +8,19 @@ int         exec_logic(t_logic_token **token);
 const char  *pipeline_parse_check(const char *command);
 int         exec_pipeline(const char *command);
 
+typedef enum e_pipeline_token_type {
+    ARG,
+    REDIRECTION,
+    PIPE,
+    PIPELINE_EOF
+} t_pipeline_token_type;
+
+typedef struct s_pipeline_token {
+    t_pipeline_token_type type;
+    char *content;
+} t_pipeline_token;
+
+t_pipeline_token    *tokenize_pipeline(char *pipeline);
+void                free_pipeline(t_pipeline_token *pipeline);
+
 #endif
