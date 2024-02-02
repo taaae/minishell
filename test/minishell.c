5a45613f841ddc27c8b7f930c438fa942078d441
Leonid Poroshin <lporoshi@student.42heilbronn.de>
2024-02-02 17:48:39 +0100

    add test executable for the main minishel program

---
+++ test/minishell.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lporoshi <lporoshi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/02 17:35:16 by lporoshi          #+#    #+#             */
+/*   Updated: 2024/02/02 17:47:07 by lporoshi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "builtins.h"
+#include "config.h"
+#include "environment.h"
+#include "exec_find.h"
+#include "expansions.h"
+#include "lexer.h"
+#include "logic_tokenizer.h"
+#include "minishell.h"
+#include "reader.h"
+#include "signals.h"
+#include "libft.h"
+
+int	main(void)
+{
+	char			*line;
+	t_logic_token	**splitted_line;
+
+	init_signal_handlers();
+	while (1)
+	{
+		line = get_line();
+		if (line == NULL)
+			return (NULL);
+		splitted_line = logic_split(line);
+		while (*splitted_line)
+		{
+			ft_printf("%s///", (*splitted_line)->strrepr);
+			splitted_line++;
+		}
+		free(line);
+	}
+}
