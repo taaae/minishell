5596c6fb6f542b8ddbdd5a475c2964221c0a8d45
Leonid Poroshin <lporoshi@student.42heilbronn.de>
2024-02-03 15:59:58 +0100

    snapshot signals

--- src/signals/signals2.c
+++ src/signals/signals2.c
@@ -6,7 +6,7 @@
 /*   By: lporoshi <lporoshi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/02 21:46:48 by lporoshi          #+#    #+#             */
-/*   Updated: 2024/02/02 21:54:48 by lporoshi         ###   ########.fr       */
+/*   Updated: 2024/02/03 15:53:13 by lporoshi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,23 @@ struct sigaction sa;
     sa.sa_flags = SA_RESTART;
 	tc(s)getattr
 */
+//
 void	sig_handler_children(int signum)
 {
-	// Handle signals here
+	if (signum == SIGINT)
+	{
+		return ;
+	}
+}
+
+void	init_sigs_for_child(void)
+{
+	struct sigaction	sa;
+
+	rl_catch_signals = 0;
+	sa.sa_handler = sig_handler_children;
+	sigemptyset(&sa.sa_mask);
+	sigaddset(&sa.sa_mask, SIGHUP);
+	sa.sa_flags = SA_RESTART;
+	sigaction(SIGINT, &sa, NULL);
 }
