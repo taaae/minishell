823da16793e646aae199547a70df4bfa58730a7b
taaae <timkingteam@gmail.com>
2024-02-03 18:58:54 +0100

    headers in heredoc

--- src/reader/heredoc_utils.c
+++ src/reader/heredoc_utils.c
@@ -10,6 +10,10 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <readline/readline.h>
+#include <readline/history.h>
 #include <fcntl.h>
 #include <errno.h>
 #include "lexer.h"
