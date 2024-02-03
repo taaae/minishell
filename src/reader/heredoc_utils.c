b2160c85a7bc75798aaa29aff7df30b45b1bc99e
taaae <timkingteam@gmail.com>
2024-02-03 20:17:38 +0100

    Revert "headers in heredoc"
    This reverts commit 823da16793e646aae199547a70df4bfa58730a7b.

--- src/reader/heredoc_utils.c
+++ src/reader/heredoc_utils.c
@@ -10,10 +10,6 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <readline/readline.h>
-#include <readline/history.h>
 #include <fcntl.h>
 #include <errno.h>
 #include "lexer.h"
