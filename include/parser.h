aa3feb3fca5cce2a79233503796c2e7e91f7f9f7
Leonid Poroshin <lporoshi@student.42heilbronn.de>
2024-02-05 13:55:25 +0100

    norm parser.h

--- include/parser.h
+++ include/parser.h
@@ -1,45 +1,57 @@
-#ifndef MINISHELL_PARSER_H
-# define MINISHELL_PARSER_H
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   signals.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lporoshi <lporoshi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/17 14:10:06 by lporoshi          #+#    #+#             */
+/*   Updated: 2024/02/03 17:10:12 by lporoshi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PARSER_H
+# define PARSER_H
 
 # include "logic_tokenizer.h"
 # include "libft.h"
 
-char        *logic_parse_check(t_logic_token **token);
-int         exec_logic(t_logic_token **token);
-char  *pipeline_parse_check(char *command);
-int         exec_pipeline(char *command);
+char				*logic_parse_check(t_logic_token **token);
+int					exec_logic(t_logic_token **token);
+char				*pipeline_parse_check(char *command);
+int					exec_pipeline(char *command);
 
 typedef enum e_pipeline_token_type {
-    ARG,
-    REDIRECTION,
-    PIPE,
-    PIPELINE_EOF
-} t_pipeline_token_type;
+	ARG,
+	REDIRECTION,
+	PIPE,
+	PIPELINE_EOF
+}	t_pipeline_token_type;
 
 typedef struct s_pipeline_token
 {
-    t_pipeline_token_type type;
-    char *content;
-} t_pipeline_token;
+	t_pipeline_token_type	type;
+	char					*content;
+}	t_pipeline_token;
 
-t_pipeline_token    *tokenize_pipeline(char *pipeline);
-void                free_pipeline(t_pipeline_token *pipeline);
+t_pipeline_token	*tokenize_pipeline(char *pipeline);
+void				free_pipeline(t_pipeline_token *pipeline);
 
 typedef enum e_arg_token_type
 {
-    NORMAL_ARG,
-    DQUOTED,
-    SQUOTED,
-} t_arg_token_type;
+	NORMAL_ARG,
+	DQUOTED,
+	SQUOTED,
+}	t_arg_token_type;
 
 typedef struct s_arg_token
 {
-    t_arg_token_type type;
-    char *content;
-} t_arg_token;
+	t_arg_token_type	type;
+	char				*content;
+}	t_arg_token;
 
-t_list  *tokenize_arg(char *arg);
-void    print_token_args(t_list *lst);
-void expand_vars(char **str);
+t_list				*tokenize_arg(char *arg);
+void				print_token_args(t_list *lst);
+void				expand_vars(char **str);
 
 #endif
